d2b4b1536f54680f27efa47638226c2371af1eb9
Karthiga <Karthiga@excelenciaconsulting.com>
2017-03-01 19:46:24 +0530

    changes route

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,11 +8,16 @@
                 <div class="panel-heading">Bechdel Test</div>
 
                 <div class="panel-body">
+                 @if(Auth::check())
                     <h2>About</h2>
                     <p>The <b>Bechdel Test</b>, sometimes called the <i>Mo Movie Measure</i> or <i>Bechdel Rule</i> is a simple test which names the following three criteria: (1) it has to have at least two women in itwho (2)talk to each other, about (3) something besides a man. The test was popularized by <b>Alison Bechdel</b>'s comic <b>Dykes to Watch Out For</b>, in a 1985 strip called <a href="http://alisonbechdel.blogspot.com/2005/08/rule.html"><i>The Rule</i></a>. For a nice video introduction to the subject please check out <a href="http://www.feministfrequency.com/2009/12/the-bechdel-test-for-women-in-movies/">The Bechdel Test for Women in Movies</a> on <a href="http://www.feministfrequency.com/">feministfrequency.com</a>.</p>
                     <h2>Add a movie</h2>
                     <p>If you want to add a movie to this list, please go to the <a href="/add" style="text-decoration: underline; font-weight: bold">Add a movie</a> page and fill in the form. If you want to correct an entry or contact me about anything, please visit the <a href="/contact/">contact</a> page. If you disagree with a rating, please leave a comment on the appropriate movie page instead.</p>
+                     @endif
                 </div>
+                 @if(Auth::guest())
+                     <a href="/login" class="btn btn-info"> You need to login to see the list </a>
+                 @endif
             </div>
         </div>
     </div>
